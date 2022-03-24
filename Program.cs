815c3915ed47b8e833669583f7521d9a4cea13c6
sontikz <101814966+sontikz@users.noreply.github.com>
2022-03-24 12:10:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1_event_2403 //for english alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reader = new Delegate();
+            reader.OnKeyPressed += Whriter;
+            reader.Run();
+        }
+        public static void Whriter(object sender, char ch)
+        {
+            Console.WriteLine(ch);
+        }
+    }
+    public class Delegate
+    {
+        public event EventHandler<char> OnKeyPressed;
+        public void Run()
+        {
+            char symb = Console.ReadKey().KeyChar;
+            while (symb != 'c')
+            {
+                OnKeyPressed?.Invoke(this, symb);
+                symb = Console.ReadKey().KeyChar;
+            }
+        }
+    }
+}
